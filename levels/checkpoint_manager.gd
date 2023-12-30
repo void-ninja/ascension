e5772e559897dd5a444f9fd7fc97aa6aadce4b34
void-ninja <cade.huelsman@gmail.com>
2023-12-29 20:08:57 -0500

    Mini commit, changed checkpoint signal
    CheckpointManager now programmatically connects all checkpoint children's hit signal to its hit signal

--- levels/checkpoint_manager.gd
+++ levels/checkpoint_manager.gd
@@ -2,6 +2,11 @@ extends Node
 
 signal checkpoint_hit(checkpoint) #passes up a reference to the checkpoint that got activated
 
+func _ready() -> void:
+	for i in get_children():
+		i.connect("checkpoint_hit",_on_checkpoint_checkpoint_hit)
+
+
 func _on_checkpoint_checkpoint_hit(checkpoint: Variant) -> void:
 	checkpoint_hit.emit(checkpoint)
 
